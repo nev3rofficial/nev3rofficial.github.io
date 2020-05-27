5e26f0ef1437e50596f94d8cc40ae9d9a4b87526
Maya Faulhaber <mayafaulhaber@MacBook-Pro.local>
2020-05-27 15:52:04 -0700

    Added bar, added basic css transitions, added colors, fixed margins, padding, etc,

---
+++ javascript/scripts.js
@@ -0,0 +1,15 @@
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function() {
+    if(isScrolledIntoView($('#theTarget')))
+    {
+        alert('visible');
+    }
+});
